6a4f0c0e78715c459296031d90f1f3cd9e59ccb2
deutschbomb <deutschbomb@mail.ru>
2025-03-12 23:16:00 +0300

    test page is ready

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,43 @@
 // Add JS here
+
+const quizForm = document.getElementById("quiz-form");
+
+// <!-- JavaScript for handling submission -->
+quizForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from submitting to a server
+
+    // Array of correct answers (A, B, B, C, A correspond to questions 1-5)
+    const correctAnswers = ["", "A", "A", "B", "B", "A", "C", "B", "B", "A", "A"];
+    const totalQuestions = 10;
+    const answers = Array.from(new FormData(quizForm).entries());
+    let correctCount = 0;
+
+    if (answers.length == correctAnswers.length - 1) {
+      answers.forEach(answer => {
+        const answerNumber = parseInt(answer[0]);
+
+        // Check each question's selected answer
+        if (answer[1] == correctAnswers[answerNumber]) {
+          correctCount++;
+        } else {
+          // Show results
+          document.getElementById("themes-repeat").style.display = "block";
+          document.getElementsByClassName(answer[0])[0].style.display = "block";
+        }
+
+        let correctPercentage = (correctCount / totalQuestions) * 100;
+
+        // Update the results display
+        document.getElementById("count").textContent = correctCount;
+        document.getElementById("percentage").textContent = correctPercentage;
+
+        // Hide quiz
+        document.getElementById("quiz").style.display = "none";
+        document.getElementById("results").style.display = "block";
+
+      });
+    }
+    else {
+      alert("Необходимо ответить на все вопросы!");
+    }
+  });
